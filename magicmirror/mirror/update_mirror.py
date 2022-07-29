f826db0da6eb343d87497b4cb6c2838029517f07
timothy-salazar <timothy.salazar@colorado.edu>
2022-07-29 16:08:13 -0600

    function to create or overwrite term.txt file

---
+++ magicmirror/mirror/update_mirror.py
@@ -0,0 +1,47 @@
+""" Receives block of text from a plugin and inserts the block of text into the
+term.txt file.
+"""
+import os
+
+def get_script_dir():
+    """ Input:
+            None
+        Output:
+            script_dir: pathlib.Path - the path of the script directory
+
+    This is just for my paranoia. It gives the path to the directory in which
+    this script resides.
+    TODO: It probably makes sense to have directory of the entire project stored
+    as an environment variable.
+    """
+    this_file_path = os.path.abspath(__file__)
+    script_dir = os.path.dirname(this_file_path)
+    return script_dir
+
+def make_term_file():
+    """ Input:
+            None
+        Output:
+            Creates or overwrites a file with enough whitespace characters to
+            completely fill the terminal.
+    This does the following:
+        - Gets the terminal width and height (in columns and rows)
+        - Opens a file in the same directory as this script named "term.txt"
+        - Fills the file with spaces, such that there are term_height lines,
+          each of which is term_width long.
+    """
+    # Width/height are in columns/lines
+    term_width, term_height = os.get_terminal_size()
+    # This makes a string that should completely cover the terminal if you print
+    # it out.
+    # Equivalent to:
+    #   display_text = ''
+    #   for y in range(term_height):
+    #       for x in range(term_width):
+    #           display_text += ' '
+    #       display_text += '\n'
+    display_text = '\n'.join([' '*term_width for i in range(term_height)])
+    script_dir = get_script_dir()
+    term_file_path = os.path.join(script_dir, 'term.txt')
+    with open(term_file_path, 'w+') as f:
+        f.write(display_text)
