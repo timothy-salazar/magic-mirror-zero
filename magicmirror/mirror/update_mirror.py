c1e0f9264742cbd2386d84995bc8101d0bfa45c6
timothy-salazar <timothy.salazar@colorado.edu>
2022-07-29 20:16:08 -0600

    function for inserting block of text into term.txt file written

--- magicmirror/mirror/update_mirror.py
+++ magicmirror/mirror/update_mirror.py
@@ -45,3 +45,44 @@ def make_term_file():
     term_file_path = os.path.join(script_dir, 'term.txt')
     with open(term_file_path, 'w+') as f:
         f.write(display_text)
+
+def insert_text_block(txt, column, row, txt_width, txt_height):
+    """ Input:
+            txt: string - a block of text that we want to insert into the
+                term.txt file. Lines are separated by newline characters.
+            column: int - the offset of the upper lefthand corner of the text
+                block (in characters, not pixels or anything). If it's all the
+                way to the left, 'column' would be 0. If the text should be
+                placed 10 columns from the left, 'column' would be 10.
+            row: int - the offset of the upper lefthand corner of the text
+                block (in rows from the start of the file). If it's all the way
+                at the top of the page, 'row' would be 0.
+            txt_width: int - how many characters wide is the text box
+            txt_width: int - how many rows does the text box take up
+        Output:
+            Edits the term.txt file
+    
+    It may be that I'll need to rewrite this.
+    I think that by doing the reading and writing all in one step I can avoid
+    issues with other bits and pieces trying to modify and/or display the
+    contents of term.txt, but I could be horrendously wrong.
+    """
+    txt_lines = txt.split('\n')
+    script_dir = get_script_dir()
+    term_file_path = os.path.join(script_dir, 'term.txt')
+    with open(term_file_path, 'r+') as f:
+        old_data = f.read().split('\n')
+        for row_number in range(len(old_data)):
+            line = old_data[row_number]
+            if row_number < row:
+                pass
+            elif row_number > (row + txt_height - 1):
+                break
+            else:
+                new_line = line[:column] \
+                    + txt_lines[row_number-row] \
+                        + line[txt_width+column:]
+                old_data[row_number] = new_line
+        new_data = '\n'.join(old_data)
+        f.seek(0)
+        f.write(new_data)
