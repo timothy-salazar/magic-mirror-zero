485a9834065c31f4e6a8ca2b3da703fbd83aef79
timothy-salazar <timothy.salazar@colorado.edu>
2022-08-07 18:26:27 -0600

    roughing out rough shape of cron_launcher

---
+++ magicmirror/mirror/cron_launcher.py
@@ -0,0 +1,12 @@
+""" This will read in the config file, pass the results as a dictionary to
+cron_formatter(), which formats each entry so they can be entered into the
+user crontab file.
+The cron_launcher() function then adds or updates each entry to the user
+crontab file.
+"""
+import configparser
+
+
+
+if __name__ == "__main__":
+    pass
